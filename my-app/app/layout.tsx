1b3d264ed866d4d2f2e9877ed1e1417aa4100a26
Mandy Sumner <122269006+arsumner@users.noreply.github.com>
2026-04-16 00:56:38 +0000

    add supabase client

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -18,10 +18,10 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+children,
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html
       lang="en"
