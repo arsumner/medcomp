4323b924bf5cdc4ff3b7dd1fcfe830512c39193c
Mandy Sumner <122269006+arsumner@users.noreply.github.com>
2026-04-21 00:50:52 +0000

    add basic navbar component

---
+++ my-app/app/components/navbar.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className="title">
+      <span className="text-white font-bold">MedComp</span>
+    </nav>
+  )
+}
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from './components/navbar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1> Hello World!</h1>
-    </div>
-  );
+    <main>
+      <Navbar />
+    </main>
+  )
 }
