1a727215f0506b2cb2d16fe2d40a46209403d895
Mandy Sumner <122269006+arsumner@users.noreply.github.com>
2026-04-26 17:07:21 +0000

    add hero, table components

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Navbar from './components/navbar'
+import Hero from './components/hero'
+import Table from './components/table/page'
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <Table />
     </main>
   )
 }
