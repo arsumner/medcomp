54cbb1a5f52ea6953627a19fb0eac4b79b034245
Amanda Sumner <sumnera@oregonstate.edu>
2026-05-02 14:57:38 -0400

    change case to match database table definitions

--- my-app/app/api/submissions/route.ts
+++ my-app/app/api/submissions/route.ts
@@ -1,4 +1,4 @@
-import { supabase } from '../../../lib/supabase';
+import { supabase } from '@/lib/supabase';
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
@@ -8,14 +8,14 @@ export async function GET(request: Request) {
     const hospital = searchParams.get('hospital')
     const sort = searchParams.get('sort')
 
-    let query = supabase.from('Submission').select(`
-        *, Role (profession, title, license_type),
-        Hospital (name, city, state)`)
+    let query = supabase.from('submission').select(`
+        *, role (profession, title, license_type),
+        hospital (name, city, state)`)
 
-    if (profession) query = query.eq('Role.profession', profession)
-    if (city) query = query.eq('Hospital.city', city)
-    if (state) query = query.eq('Hospital.state', state)
-    if (hospital) query = query.eq('Hospital.name', hospital)
+    if (profession) query = query.eq('role.profession', profession)
+    if (city) query = query.eq('hospital.city', city)
+    if (state) query = query.eq('hospital.state', state)
+    if (hospital) query = query.eq('hospital.name', hospital)
     if (sort == 'asc') query = query.order('base_rate', { ascending: true })
     if (sort == 'desc') query = query.order('base_rate', { ascending: false })
     if (!sort) query = query.order('submitted_at', { ascending: false})
@@ -29,7 +29,7 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
     const body = await request.json()
-    const { data, error } = await supabase.from('Submission').insert([
+    const { data, error } = await supabase.from('submission').insert([
         {
         pay_type: body.pay_type, night_diff: body.night_diff, charge_diff: body.charge_diff, 
         evening_diff: body.evening_diff, preceptor_pay: body.preceptor_pay, certification_pay: body.certification_pay,
